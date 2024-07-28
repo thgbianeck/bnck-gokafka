cbf7b7cfe07e8f150681d94f28b7530533ee266f
Thaigo Bianeck <thgbianeck@gmail.com>
2024-07-28 15:25:13 -0300

    Finalizando estudos

---
+++ cmd/producer/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"log"
+)
+
+func main() {
+	deliveryChan := make(chan kafka.Event)
+	producer := NewKafkaProducer()
+	Publish("transferiu", "teste", producer, []byte("transferecia2"), deliveryChan)
+	DeliveryReport(deliveryChan) // async
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers":   "bnck-gokafka-kafka-1:9092",
+		"delivery.timeout.ms": "0",
+		"acks":                "all",
+		"enable.idempotence":  "true",
+	}
+	p, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return p
+}
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
+	message := &kafka.Message{
+		Value:          []byte(msg),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            key,
+	}
+	err := producer.Produce(message, deliveryChan)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeliveryReport(deliveryChan chan kafka.Event) {
+	for e := range deliveryChan {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				fmt.Println("Erro ao enviar")
+			} else {
+				fmt.Println("Mensagem enviada:", ev.TopicPartition)
+				// anotar no banco de dados que a mensagem foi processado.
+				// ex: confirma que uma transferencia bancaria ocorreu.
+			}
+		}
+	}
+}
